08c38da3cfdb2f5355853bd613e09e77f2c8e195
Lovish Bansal <lovishbansal441@gmail.com>
2024-11-16 19:05:00 +0530

    [render]

--- backend/app.ts
+++ backend/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-            'https://complyanceio.onrender.com',
+            'https://complyanceio.vercel.app',
             'http://localhost:5173',
         ],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
